f236a7815f676b790fabcd766101e3d7721bfaa8
Asmita Chatterjee <baliaschat9@gmail.com>
2021-01-29 19:55:28 +0530

    multiplayer game

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
 var ball;
+var db,position;
 
 function setup(){
     createCanvas(500,500);
+    db=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var positionRef=db.ref("ball/position");
+    positionRef.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    var positionRef=db.ref("ball/position");
+    positionRef.set({
+        x:ball.x + x,
+        y:ball.y+y
+    })
+}
+function showError(){
+    console.log("Error has occured");
+}
+function readPosition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
 }
