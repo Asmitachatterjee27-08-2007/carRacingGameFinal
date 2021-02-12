f8c29978efb8ce19cc49127e152c9062d101e02b
Asmita Chatterjee <baliaschat9@gmail.com>
2021-02-12 21:18:28 +0530

    Form created

--- sketch.js
+++ sketch.js
@@ -1,44 +1,19 @@
-var ball;
-var db,position;
+
+var db,gameState=0,playerCount;
+
+var form,player,game;
 
 function setup(){
     createCanvas(500,500);
     db=firebase.database();
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    var positionRef=db.ref("ball/position");
-    positionRef.on("value",readPosition,showError);
+    game= new Game();
+    game.getState();
+    game.start();
+
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
+    
+    
 }
 
-function writePosition(x,y){
-    var positionRef=db.ref("ball/position");
-    positionRef.set({
-        x:ball.x + x,
-        y:ball.y+y
-    })
-}
-function showError(){
-    console.log("Error has occured");
-}
-function readPosition(data){
-    position=data.val();
-    ball.x=position.x;
-    ball.y=position.y;
-}
